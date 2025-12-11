2c0f1741cbe212aac8c2d66b2abaeb05163e8666
ducanh2206aa <email-ca-ducanh2206aa@gmail.com>
2025-12-11 22:31:30 +0700

    fix upload image

--- commons/function.php
+++ commons/function.php
@@ -4,7 +4,8 @@ if (!defined('DB_HOST')) {
 }
 
 // Kết nối CSDL qua PDO
-function connectDB() {
+function connectDB()
+{
     // Kết nối CSDL
     $host = DB_HOST;
     $port = DB_PORT;
@@ -18,27 +19,32 @@ function connectDB() {
 
         // cài đặt chế độ trả dữ liệu
         $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    
+
         return $conn;
     } catch (PDOException $e) {
         echo ("Connection failed: " . $e->getMessage());
     }
 }
 
-function uploadFile($file, $folderSave){
+function uploadFile($file, $folderSave)
+{
     $file_upload = $file;
     $pathStorage = $folderSave . rand(10000, 99999) . $file_upload['name'];
 
     $tmp_file = $file_upload['tmp_name'];
-    $pathSave = PATH_ROOT . $pathStorage; // Đường dãn tuyệt đối của file
-
+    $pathSave = PATH_ROOT . $pathStorage; 
+    $dir = dirname($pathSave);
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
     if (move_uploaded_file($tmp_file, $pathSave)) {
         return $pathStorage;
     }
     return null;
 }
 
-function deleteFile($file){
+function deleteFile($file)
+{
     $pathDelete = PATH_ROOT . $file;
     if (file_exists($pathDelete)) {
         unlink($pathDelete); // Hàm unlink dùng để xóa file
